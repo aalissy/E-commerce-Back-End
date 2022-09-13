bfd371ebf14a1f16bd769fe693ad58591b19b26c
aalissy <32420245+aalissy@users.noreply.github.com>
2022-09-12 19:54:53 -0500

    Added comments

--- models/Category.js
+++ models/Category.js
@@ -7,12 +7,14 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    // Defines id as an integer, doesn't allow it to be null, sets it to a primary key, and makes it automatically increment its values
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
+    // Defines category name as a string and doesn't allow it to be null
     category_name: {
       type: DataTypes.STRING,
       allowNull: false,
