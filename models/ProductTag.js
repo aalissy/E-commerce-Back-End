27d8d71a893e55a63c9dcc4fbc45b3f4c7b38e52
aalissy <32420245+aalissy@users.noreply.github.com>
2022-09-12 20:02:23 -0500

    Added comments

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,12 +7,14 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    // Defines id as an integer, doesn't allow it to be null, sets it as the primary key, and makes it automatically increment its values
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
+    // Defines product id as an integer and references the product model using the id as the key
     product_id: {
       type: DataTypes.INTEGER,
       references: {
@@ -20,6 +22,7 @@ ProductTag.init(
         key: "id",
       },
     },
+    // Defines tag id as an integer and references the tag model using the id as the key
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
