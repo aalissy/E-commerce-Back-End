85df79a6b4aea809029d04a011ccab57dadd7812
aalissy <32420245+aalissy@users.noreply.github.com>
2022-09-12 20:01:04 -0500

    Added comments

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,7 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-    // Defines id as an integer, doesn't allow it to be null, sets it as a primary key, and makes it automatically increment its values
+    // Defines id as an integer, doesn't allow it to be null, sets it as the primary key, and makes it automatically increment its values
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
